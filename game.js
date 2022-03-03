b7801ca5253f9d4730f3b2232d741ad038da7087
Joe Rupp <joerpp8@gmail.com>
2022-03-03 11:15:24 -0700

    Add class functionality

--- game.js
+++ game.js
@@ -1,5 +1,43 @@
 class Game {
   constructor() {
+    this.difficultyClassic = '';
+    this.user = new Player('You');
+    this.userChampion = '';
+    this.enemy = new Player('Enemy');
+    this.enemyChampion = '';
+    this.outcome = '';
+  }
 
+  chooseDifficulty(difficultyChoice) {
+    if (difficultyChoice === 'Extreme') {
+      this.difficultyClassic = 'Extreme';
+    } else {
+      this.difficultyClassic = 'Classic';
+    }
+  }
+
+  battle(userChoice, enemyChoice) {
+    if (userChoice === "Fire" && enemyChoice === "Earth" || "Void") {
+      var outcome = 'Winner';
+      this.user.battleOutcome(outcome);
+    } else if (userChoice === "Earth" && enemyChoice === "Water" || "Air") {
+      var outcome = 'Winner';
+      this.user.battleOutcome(outcome);
+    } else if (userChoice === "Water" && enemyChoice === "Fire" || "Air") {
+      var outcome = 'Winner';
+      this.user.battleOutcome(outcome);
+    } else if (userChoice === "Air" && enemyChoice === "Void" || "Fire") {
+      var outcome = 'Winner';
+      this.user.battleOutcome(outcome);
+    } else if (userChoice === "Void" && enemyChoice === "Earth" || "Water") {
+      var outcome = 'Winner';
+      this.user.battleOutcome(outcome);
+    } else if (userChoice === enemyChoice) {
+      var outcome = 'Tie';
+      this.user.battleOutcome(outcome);
+    } else {
+      var outcome = 'Loser';
+      this.user.battleOutcome(outcome);
+    }
   }
 };
--- player.js
+++ player.js
@@ -1,5 +1,32 @@
 class Player {
-  constructor() {
+  constructor(name) {
+    this.name = name;
+    this.numOfWins = 0;
+    this.champion = '';
+  }
 
+  chooseChampion(selection) {
+    if (selection === 'Fire') {
+      this.champion = 'Fire';
+    } else if (selection === 'Water') {
+      this.champion = 'Water';
+    } else if (selection === 'Earth') {
+      this.champion = 'Earth';
+    } else if (selection === 'Air') {
+      this.champion = 'Air';
+    } else (selection === 'Void') {
+      this.champion = 'Void';
+    }
+  }
+
+  battleOutcome(outcome) {
+    if (outcome === 'Winner') {
+      this.numOfWins++;
+      return 'You Won!';
+    } else if (outcome === 'Tie') {
+      return 'Tie!';
+    } else if (outcome === 'Loser'){
+      return 'You Lost!';
+    }
   }
 };
